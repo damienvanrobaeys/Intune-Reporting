0bee5a9e1400c985f7e22cd716b5f3b1da83386f
Damien Van Robaeys <damien.vanrobaeys@gmail.com>
2025-01-28 09:32:02 +0100

    Add files via upload

--- Send Intune DiscoveredApps to Log Analytics/Export discoveredapps to Log Analytics/DiscoveredApps_LogAnalytics.ps1
+++ Send Intune DiscoveredApps to Log Analytics/Export discoveredapps to Log Analytics/DiscoveredApps_LogAnalytics.ps1
@@ -1,13 +1,30 @@
-#$Custom_Logs = "Export_DiscoveredApps"
+# Log Analytics info
 $CustomerId = "" # Log Analytics Workspace ID
 $SharedKey = '' # Log Analytics Workspace Primary Key
 $TimeStampField = ""
 $Custom_log = "Intune_DiscoveredApps"
 
-$ClientID = ""
-$Secret = ''    
-$Site_URL = ""
-$Folder_Location = ""
+# SharePoint info
+$Tenant = ""  # tenant name
+$ClientID = "" # azure app client id 
+$Secret = '' # azure app secret
+$SharePoint_SiteID = ""  # sharepoint site id	
+$SharePoint_Path = "https://grtgaz.sharepoint.com/sites/DWP-Support/Documents%20partages"  # sharepoint main path
+# Somethinhg like "https://systanddeploy.sharepoint.com/sites/Support/Documents%20partages"
+$SharePoint_ExportFolder = "Windows/Apps_Report"  # folder where to upload file
+# Something like "Windows/Apps_Report"
+$CSV_DiscoveredApps_Windows = "DiscoveredApps_Windows.csv"
+$FileName = $CSV_DiscoveredApps_Windows.Split("\")[-1]  
+
+<#
+Now to get the ID of a SharePoint site proceed as below:
+1. Open your browser
+2. Type the following URL: 
+https://yoursharepoint.sharepoint.com/sites/yoursite/_api/site/id
+
+In my case it's:
+https://systanddeploy.sharepoint.com/sites/Support/_api/site/id
+#>
 
 # Log analytics functions
 Function Build-Signature ($customerId, $sharedKey, $date, $contentLength, $method, $contentType, $resource)
@@ -55,16 +72,42 @@ Function Post-LogAnalyticsData($customerId, $sharedKey, $body, $logType)
     return $response.StatusCode
 }
 
-Connect-PnPOnline -Url $Site_URL -ClientId $ClientID -ClientSecret $Secret -WarningAction Ignore
+$Body = @{  
+	client_id = $ClientID
+	client_secret = $Secret
+	scope = "https://graph.microsoft.com/.default"   
+	grant_type = 'client_credentials'  
+}  
+	
+$Graph_Url = "https://login.microsoftonline.com/$($Tenant).onmicrosoft.com/oauth2/v2.0/token"  
+$AuthorizationRequest = Invoke-RestMethod -Uri $Graph_Url -Method "Post" -Body $Body  
 
-$Get_WindowsApps_CSV = Get-PnPFolderItem -FolderSiteRelativeUrl "Documents partages/Windows/Apps_Report" | where {$_.Name -like "DiscoveredApps_Windows.csv*"}
+$Access_token = $AuthorizationRequest.Access_token  
+$Header = @{  
+	Authorization = $AuthorizationRequest.access_token  
+	"Content-Type"= "application/json"  
+	'Content-Range' = "bytes 0-$($fileLength-1)/$fileLength"	
+}  
 
-$CSV_URL = $Get_WindowsApps_CSV.ServerRelativeUrl
-Get-PnPFile -Url $CSV_URL -Path $env:temp -FileName "DiscoveredApps_Windows.csv" -AsFile -Force
+$SharePoint_Graph_URL = "https://graph.microsoft.com/v1.0/sites/$SharePoint_SiteID/drives"  
+$BodyJSON = $Body | ConvertTo-Json -Compress  
+
+$Result = Invoke-RestMethod -Uri $SharePoint_Graph_URL -Method 'GET' -Headers $Header -ContentType "application/json"   
+$DriveID = $Result.value| Where-Object {$_.webURL -eq $SharePoint_Path } | Select-Object id -ExpandProperty id  
+$FileName = $CSV_DiscoveredApps_Windows.Split("\")[-1]  
+$fileurl = "https://graph.microsoft.com/v1.0/sites/$SharePoint_SiteID/drives/$DriveID/root:/$SharePoint_ExportFolder/$($fileName):/content"
 $File_Path = "$env:temp\DiscoveredApps_Windows.csv"
 
-$InputFilename = [System.IO.File]::ReadAllLines($File_Path)
+Try
+	{
+		Invoke-RestMethod -Headers $Header -Uri $fileurl -OutFile $File_Path		
+	}
+Catch
+	{
+		EXIT
+	}
 
+$InputFilename = [System.IO.File]::ReadAllLines($File_Path)
 $OutputFilenamePattern = ".\Export_"
 $LineLimit = 1000
 $line = 0
